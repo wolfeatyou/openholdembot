b5ae46ac418615759188b367f50c62e77b1aacff
wolfeatyou <fivefong@gmail.com>
2015-04-27 21:16:59 +0300

    eee

--- OpenHoldem/CLazyScraper.cpp
+++ OpenHoldem/CLazyScraper.cpp
@@ -20,10 +20,12 @@
 #include "CScraper.h"
 #include "CScraperAccess.h"
 #include "CSymbolEngineHistory.h"
+#include "CSymbolEngineTime.h"
 #include "CSymbolEngineIsTournament.h"
 #include "CSymbolEngineGameType.h"
 #include "CSymbolEngineUserchair.h"
 #include "CTableState.h"
+
 #include "debug.h"
 
 CLazyScraper *p_lazyscraper = NULL;
@@ -185,7 +187,8 @@ bool CLazyScraper::NeedAllPlayerNames() {
 	// It is enough if we do this until our turn, because
 	// * at our turn we have stable frames
 	// * new players after our turn can't affect the current hand
-	return (!p_symbol_engine_history->DidActThisHand());
+	//return (!p_symbol_engine_history->DidActThisHand());
+	return (p_symbol_engine_time->elapsedhand() <= 3) ;
 }
 
 bool CLazyScraper::NeedUnknownPlayerNames() {
@@ -200,12 +203,12 @@ bool CLazyScraper::NeedCommunityCards() {
 }
 
 void CLazyScraper::ScrapeUnknownPlayerNames() {
-	for (int i=0; i<p_tablemap->nchairs(); i++) {
-		if (p_scraper_access->IsPlayerSeated(i) 
-			  && (p_table_state->_players[i]._name == "")) {
-			p_scraper->ScrapeName(i);
-		}
-	}
+ 	for (int i=0; i<p_tablemap->nchairs(); i++) {
+		if (	p_scraper_access->IsPlayerSeated(i) 
+			&&	!p_scraper_access->IsGoodPlayername(i)) {
+ 			p_scraper->ScrapeName(i);
+ 		}
+ 	}
 }
 
 bool CLazyScraper::NeedColourCodes() {
